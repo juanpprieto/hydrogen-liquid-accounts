0f634c8aa94b2f6863f38d9307ad946acc2692b0
Juan P. Prieto <jp@calltheguys.co>
2024-06-07 06:55:22 -0700

    Add liquid proxying catch all route

--- app/routes/$.tsx
+++ app/routes/$.tsx
@@ -1,9 +1,42 @@
-import type {LoaderFunctionArgs} from '@shopify/remix-oxygen';
+import type {
+  LoaderFunctionArgs,
+  ActionFunctionArgs,
+} from '@shopify/remix-oxygen';
 
-export async function loader({request}: LoaderFunctionArgs) {
-  throw new Response(`${new URL(request.url).pathname} not found`, {
-    status: 404,
-  });
+export async function action(args: ActionFunctionArgs) {
+  return await proxyRequestToLiquid(args);
+}
+
+export async function loader(args: LoaderFunctionArgs) {
+  const response = await proxyRequestToLiquid(args);
+
+  if (!response.ok) {
+    throw new Response(`${new URL(args.request.url).pathname} not found`, {
+      status: 404,
+    });
+  }
+
+  return response;
+}
+
+async function proxyRequestToLiquid({request, context}: LoaderFunctionArgs) {
+  const {env} = context;
+
+  const {pathname, search} = new URL(request.url);
+  const liquidUrl = `${env.PUBLIC_LIQUID_STORE_URL}${pathname}${search}`;
+
+  const clonedRequest = request.clone();
+
+  const requestInit: RequestInit = {
+    method: request.method,
+    headers: request.headers,
+  };
+
+  if (request.method !== 'GET') {
+    requestInit.body = await clonedRequest.arrayBuffer();
+  }
+
+  return fetch(liquidUrl, requestInit);
 }
 
 export default function CatchAllPage() {
